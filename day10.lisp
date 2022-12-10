ff84adb9c2db003b9f0ad84e27b31f6530524b7a
gingeralesy <gingeralesy@gmail.com>
2022-12-10 10:52:53 +0200

    Day 10, part 2

--- day10.lisp
+++ day10.lisp
@@ -8,32 +8,63 @@ Author: Janne Pakarinen <gingeralesy@gmail.com>
 
 (defparameter *day10-command-re* (cl-ppcre:create-scanner "^(noop|addx)( (-?\\d+))?$"))
 
+(defun day10-parse-command (line)
+  (when line
+    (multiple-value-bind (match groups)
+        (cl-ppcre:scan-to-strings *day10-command-re* line)
+      (unless match (error "Invalid command: ~a" line))
+      (let ((command (intern (format NIL "~:@(~a~)" (aref groups 0)) :keyword))
+            (argument (aref groups 2)))
+        (list command
+              (when argument (parse-integer argument))
+              (ecase command
+                (:noop 1)
+                (:addx 2)))))))
+
 (defun day10-puzzle1 ()
-  (flet ((parse-command (line)
-           (when line
-             (multiple-value-bind (match groups)
-                 (cl-ppcre:scan-to-strings *day10-command-re* line)
-               (unless match (error "Invalid command: ~a" line))
-               (let ((command (intern (format NIL "~:@(~a~)" (aref groups 0)) :keyword))
-                     (argument (aref groups 2)))
-                 (list command
-                       (when argument (parse-integer argument))
-                       (ecase command
-                         (:noop 1)
-                         (:addx 2))))))))
-    (with-open-file (stream (input 10) :if-does-not-exist :error)
-      (loop with x = 1
-            with signal = 0
-            with next = 20
-            with cycle = 0
-            for (command argument time) = (parse-command (clean (read-line stream NIL)))
+  (with-open-file (stream (input 10) :if-does-not-exist :error)
+    (loop with x = 1
+          with signal = 0
+          with next = 20
+          with cycle = 0
+          for (command argument time) = (day10-parse-command (clean (read-line stream NIL)))
+          while command
+          do (incf cycle time)
+          do (when (<= next cycle)
+               (incf signal (* next x))
+               (incf next 40))
+          do (ecase command
+               (:noop) ;; Do nothing.
+               (:addx (incf x argument)))
+          finally (return signal))))
+
+;; 14060
+
+(defun day10-puzzle2 ()
+  (with-open-file (stream (input 10) :if-does-not-exist :error)
+    (let ((stack))
+      (loop for (command argument time) = (day10-parse-command (clean (read-line stream NIL)))
             while command
-            when command
-            do (incf cycle time)
-            do (when (<= next cycle)
-                 (incf signal (* next x))
-                 (incf next 40))
+            do (dotimes (i (1- time))
+                 (push (cons :noop NIL) stack))
+            do (push (cons command argument) stack))
+      (loop with x = 1
+            for cycle from 0
+            for (command . argument) in (nreverse stack)
+            when (and (< 0 cycle) (= 0 (mod cycle 40)))
+            do (format T "~%")
+            do (format T "~c" (if (and (<= x (1+ (mod cycle 40)))
+                                       (<= (- (1+ (mod cycle 40)) 2) x))
+                                  #\# #\.))
             do (ecase command
-                 (:noop) ;; Do nothing.
-                 (:addx (incf x argument)))
-            finally (return signal)))))
+                 (:noop)
+                 (:addx (incf x argument)))))))
+
+;; PAPKFKEJ
+;;
+;; ###...##..###..#..#.####.#..#.####...##.
+;; #..#.#..#.#..#.#.#..#....#.#..#.......#.
+;; #..#.#..#.#..#.##...###..##...###.....#.
+;; ###..####.###..#.#..#....#.#..#.......#.
+;; #....#..#.#....#.#..#....#.#..#....#..#.
+;; #....#..#.#....#..#.#....#..#.####..##..
