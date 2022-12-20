4bc2b0996383cd29ea495a98205abc5147ff23c5
gingeralesy <gingeralesy@gmail.com>
2022-12-20 11:37:17 +0200

    Day 20, part 2

--- day20.lisp
+++ day20.lisp
@@ -11,7 +11,9 @@ Author: Janne Pakarinen <gingeralesy@gmail.com>
     (loop for line = (clean (read-line stream NIL))
           while line
           collect (parse-integer line) into numbers
-          finally (return (make-array (length numbers) :initial-contents numbers)))))
+          finally (return (make-array (length numbers)
+                                      :element-type 'integer
+                                      :initial-contents numbers)))))
 
 (defun day20-index (index length)
   (cond
@@ -22,12 +24,15 @@ Author: Janne Pakarinen <gingeralesy@gmail.com>
     ((<= length index) (mod index length))
     (T index)))
 
-(defun day20-mix ()
+(defun day20-mix (&optional (key 1) (rounds 1))
   (let* ((numbers (day20-parse-input))
          (length (array-dimension numbers 0))
-         (indices (loop for i from 0 below length
-                        collecting i into indices
-                        finally (return (make-array length :initial-contents indices)))))
+         (indices (make-array length :element-type 'integer
+                                     :initial-element 0)))
+    (dotimes (i length)
+      (setf (aref numbers i) (* key (aref numbers i))))
+    (dotimes (i length)
+      (setf (aref indices i) i))
     (labels ((swap (from to)
                (let* ((from (day20-index from length))
                       (to (day20-index to length))
@@ -43,10 +48,11 @@ Author: Janne Pakarinen <gingeralesy@gmail.com>
                  (let* ((ix1 (if (< 0 amount) (+ from i) (- from i)))
                         (ix2 (if (< 0 amount) (1+ ix1) (1- ix1))))
                    (swap ix1 ix2)))))
-      (dotimes (i length)
-        (let* ((position (position i indices))
-               (value (aref numbers position)))
-          (move position value)))
+      (dotimes (round rounds)
+        (dotimes (i length)
+          (let* ((position (position i indices))
+                 (value (aref numbers position)))
+            (move position value))))
       (let* ((zero-ix (position 0 numbers))
              (1k-ix (day20-index (+ zero-ix 1000) length))
              (2k-ix (day20-index (+ zero-ix 2000) length))
@@ -63,3 +69,11 @@ Author: Janne Pakarinen <gingeralesy@gmail.com>
     (+ x y z)))
 
 ;; 7228
+
+(defun day20-puzzle2 ()
+  (destructuring-bind (x y z)
+      (day20-mix 811589153 10)
+    (format T "~d,~d,~d~%" x y z)
+    (+ x y z)))
+
+;; 4526232706281
