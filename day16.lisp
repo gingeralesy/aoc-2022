1b8e18fc860fbb8011756660397cef946ff92f2f
Janne Pakarinen <gingeralesy@gmail.com>
2022-12-28 23:34:58 +0200

    Day 16, part 2. I'm surprised this worked

--- day16.lisp
+++ day16.lisp
@@ -38,7 +38,8 @@ Author: Janne Pakarinen <gingeralesy@gmail.com>
                                    (list valve (cons valve 0))
                                    (loop for tunnel in tunnels
                                          appending (list tunnel (cons tunnel 1))))))))
-          finally (return map))))
+          when (< 0 rate) collect valve into relevant
+          finally (return (values map relevant)))))
 
 (defun day16-update-table (current target through distance valves)
   (declare (type hash-table current))
@@ -117,79 +118,33 @@ Author: Janne Pakarinen <gingeralesy@gmail.com>
                            (nconc (list (gethash :valve current)) max-route))))))
 
 (defun day16-puzzle1 ()
-  (let* ((valves (day16-parse-input))
-         (closed (loop for valve in (alexandria:hash-table-values valves)
-                       when (< 0 (gethash :rate valve)) collect (gethash :valve valve))))
+  (multiple-value-bind (valves closed)
+      (day16-parse-input)
     (day16-seek-route valves :aa closed 0 0 30)))
 
 ;; 2124
 
-;; FIXME: This doesn't work. A bit of an experiment. Didn't work earlier either though, so
-;;        I'm leaving it in. Fix it someday.
+(defun day16-permutations (valves length)
+  (unless (< 0 length) (return-from day16-permutations))
+  (loop for current = valves then rest
+        for rest = (rest current)
+        while (<= length (length current))
+        for results = (day16-permutations rest (1- length))
+        appending (if results
+                      (loop for result in results
+                            collecting (nconc (list (car current)) result))
+                      (list (list (car current))))))
 
-(defun day16-route (from to valves)
-  (declare (type keyword to))
-  (declare (type hash-table valves))
-  (let ((from (etypecase from (keyword (gethash from valves)) (hash-table from))))
-    (when (< 0 (day16-find-distance from to valves))
-      (loop for current = from then (gethash through valves)
-            for table = (gethash :table current)
-            for through = (car (gethash to table))
-            collect through
-            until (eql to through)))))
-
-(defun day16-best (from closed valves time)
-  (loop with max = 0
-        with best = NIL
-        for option in closed
-        for valve = (gethash option valves)
-        for distance = (day16-find-distance from option valves)
-        for produces = (* (gethash :rate valve) (- time (1+ distance)))
-        when (or (null best) (< max produces))
-        do (setf max produces
-                 best option)
-        finally (return ;; Check for others along the way.
-                  (loop for alternative in (day16-route from best valves)
-                        until (find alternative closed)
-                        finally (return alternative)))))
-
-(defun day16-traverse (valves state-a state-b closed rate pressure time)
-  (when (<= time 0) (error "No time!"))
-  (unless closed
-    (let ((state (if (< (cdr state-a) (cdr state-b)) state-b state-a))
-          (pressure pressure)
-          (rate rate)
-          (time time))
-      (when (and (< 0 (cdr state)) (< (cdr state) time))
-        (incf pressure (* rate (cdr state)))
-        (decf time (cdr state))
-        (incf rate (gethash :rate (gethash (car state) valves))))
-      (return-from day16-traverse (+ pressure (* rate time)))))
-  (loop with (current . other) = (if (< (cdr state-a) (cdr state-b))
-                                     (cons state-a state-b)
-                                     (cons state-b state-a))
-        repeat 1
-        for option = (day16-best (car current) closed valves time)
-        for new-closed = (remove option closed)
-        for distance = (day16-find-distance (car current) option valves)
-        for new = (cons option (1+ distance))
-        for target = (gethash (if (< (cdr new) (cdr other)) (car new) (car other)) valves)
-        for time-spent = (min (cdr new) (cdr other))
-        for new-time = (- time time-spent)
-        for new-rate = (+ rate (gethash :rate target))
-        for new-pressure = (+ pressure (* time-spent rate))
-        maximizing (if (< 0 new-time)
-                       (day16-traverse
-                        valves
-                        (cons (car new) (- (cdr new) time-spent))
-                        (cons (car other) (- (cdr other) time-spent))
-                        new-closed new-rate new-pressure new-time)
-                       (day16-traverse valves current other NIL rate pressure time))))
+(defun day16-rest (valves sub-list)
+  (loop for valve in valves
+        unless (find valve sub-list) collect valve))
 
 (defun day16-puzzle2 ()
-  (let* ((valves (day16-parse-input))
-         (closed (loop for valve in (alexandria:hash-table-values valves)
-                       when (< 0 (gethash :rate valve)) collect (gethash :valve valve))))
-    (day16-traverse valves (cons :aa 0) (cons :aa 0)
-                    (sort closed #'> :key #'(lambda (x) (gethash :rate (gethash x valves))))
-                    0 0 26)))
+  (multiple-value-bind (valves closed)
+      (day16-parse-input)
+    (loop for a in (day16-permutations closed (floor (length closed) 2))
+          for b = (day16-rest closed a)
+          maximizing (+ (day16-seek-route valves :aa a 0 0 26)
+                        (day16-seek-route valves :aa b 0 0 26)))))
+
+;; 2775
